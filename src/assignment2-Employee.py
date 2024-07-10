3af57e75b8d23365bedd9b0b7fc0976f724d61f9
ndesirazu <desirazu@american.edu>
2024-07-10 12:12:51 -0400

    Update assignment2-Employee.py

--- src/assignment2-Employee.py
+++ src/assignment2-Employee.py
@@ -1,4 +1,4 @@
 class Employee():
-    def __init__(self, fname, lname, dob, empID, Yr_of_join, dept, supername, curr_sal, last_inc_dt, last_inc_per):
+    def __init__(self, fname, lname, dob, empID, yr_of_join, dept, supername, curr_sal, last_inc_dt, last_inc_per):
         pass
     
